f92c56fae13e83800c5e6223c7e03e77c40b0e8d
Meet240903 <meetmangroliya24@gmail.com>
2024-09-11 10:37:55 +0530

    add(11/9/24)

--- src/components/playKidsComponents/KidsMediaPage.js
+++ src/components/playKidsComponents/KidsMediaPage.js
@@ -1,64 +1,7 @@
 import React from 'react';
 import '../../assets/css/kidsComponentscss/kidsMediaPage.css';
-
-const mediaData = [
-  {
-    type: 'video',
-    title: 'Luigi’s Mansion 2 HD – Overview Trailer — Nintendo Switch',
-    thumbnail: 'path_to_image_1',
-    color: 'green',
-    gridRowSpan: 'span 2',
-  },
-  {
-    type: 'video',
-    title: 'Paper Mario: The Thousand-Year Door — Launch Trailer — Nintendo Switch',
-    thumbnail: 'path_to_image_2',
-    color: 'gray',
-    gridColumnSpan: 'span 1',
-    gridRowSpan: 'span 2',
-  },
-  {
-    type: 'video',
-    title: 'Endless Ocean Luminous — Launch Trailer — Nintendo Switch',
-    thumbnail: 'path_to_image_3',
-    color: 'blue',
-    gridColumnSpan: 'span 2',
-    gridRowSpan: 'span 1',
-  },
-  {
-    type: 'video',
-    title: 'Yoshi & Yoshi & The Egg | LEGO Super Mario',
-    thumbnail: 'path_to_image_4',
-    color: 'red',
-    gridColumnSpan: 'span 2',
-    gridRowSpan: 'span 2',
-  },
-  {
-    type: 'wallpaper',
-    title: 'Underwater scene',
-    thumbnail: 'path_to_image_5',
-    color: 'white',
-    gridColumnSpan: 'span 2',
-    gridRowSpan: 'span 2',
-  },
-  {
-    type: 'wallpaper',
-    title: 'On display!',
-    thumbnail: 'path_to_image_6',
-    color: 'brown',
-    gridColumnSpan: 'span 2',
-    gridRowSpan: 'span 1',
-  },
-  {
-    type: 'video',
-    title: 'Bowser Gets Bounced! LEGO Super Mario',
-    thumbnail: 'path_to_image_7',
-    color: 'red',
-    gridColumnSpan: 'span 1',
-    gridRowSpan: 'span 1',
-  },
-  // Add more objects as needed
-];
+import vdoPushIcon from '../../assets/images/kidsComponentsImgs/kidsMediaPageImgs/vdoPushIcon.png'
+import kidsMediaPageData from '../../data/kidsComponentsData/KidsMediaPageData';
 
 const KidsMediaPage = () => {
   return (
@@ -68,17 +11,25 @@ const KidsMediaPage = () => {
       </div>
 
       <div className='media-grid'>
-        {mediaData.map((media, index) => (
+        {kidsMediaPageData.map((media, index) => (
           <div
             key={index}
-            className='media-item'
+            className={`${media?.gridColumnSpan === 'span 2' && media?.gridRowSpan === 'span 1' ? 'media-flex-item' : 'media-item'}`}
             style={{
-              backgroundColor: media.color,
+              backgroundColor: media.backgroundColor,
               gridColumn: media.gridColumnSpan,
               gridRow: media.gridRowSpan,
+              color: media.color
             }}
           >
-            <img src={media.thumbnail} alt={media.title} className='media-thumbnail' />
+            {
+              media?.type === 'video' && <div className={`${media?.gridColumnSpan === 'span 2' && media?.gridRowSpan === 'span 1' ? 'kids-media-flex-vdo-push-icon' : 'kids-media-vdo-push-icon'}`}>
+                <img src={vdoPushIcon} alt='video-push-icon' />
+              </div>
+            }
+            <div className='kids-media-img-section'>
+              <img src={media.thumbnail} alt={media.title} className='media-thumbnail' />
+            </div>
             <div className='media-content'>
               <h3>{media.title}</h3>
               {media.type === 'video' && <span className='media-type'>VIDEO</span>}
---
+++ src/data/kidsComponentsData/KidsMediaPageData.js
@@ -0,0 +1,207 @@
+import sectionImg1 from '../../assets/images/kidsComponentsImgs/kidsMediaPageImgs/sectionImg1.jpg'
+import sectionImg2 from '../../assets/images/kidsComponentsImgs/kidsMediaPageImgs/sectionImg2.jpg'
+import sectionImg3 from '../../assets/images/kidsComponentsImgs/kidsMediaPageImgs/sectionImg3.jpg'
+import sectionImg4 from '../../assets/images/kidsComponentsImgs/kidsMediaPageImgs/sectionImg4.jpg'
+import sectionImg5 from '../../assets/images/kidsComponentsImgs/kidsMediaPageImgs/sectionImg5.jpg'
+import sectionImg6 from '../../assets/images/kidsComponentsImgs/kidsMediaPageImgs/sectionImg6.jpg'
+import sectionImg7 from '../../assets/images/kidsComponentsImgs/kidsMediaPageImgs/sectionImg7.jpg'
+import sectionImg8 from '../../assets/images/kidsComponentsImgs/kidsMediaPageImgs/sectionImg8.jpg'
+import sectionImg9 from '../../assets/images/kidsComponentsImgs/kidsMediaPageImgs/sectionImg9.jpg'
+import sectionImg10 from '../../assets/images/kidsComponentsImgs/kidsMediaPageImgs/sectionImg10.jpg'
+import sectionImg11 from '../../assets/images/kidsComponentsImgs/kidsMediaPageImgs/sectionImg11.jpg'
+import sectionImg12 from '../../assets/images/kidsComponentsImgs/kidsMediaPageImgs/sectionImg12.png'
+import sectionImg13 from '../../assets/images/kidsComponentsImgs/kidsMediaPageImgs/sectionImg13.jpg'
+import sectionImg14 from '../../assets/images/kidsComponentsImgs/kidsMediaPageImgs/sectionImg14.jpg'
+import sectionImg15 from '../../assets/images/kidsComponentsImgs/kidsMediaPageImgs/sectionImg15.jpg'
+import sectionImg16 from '../../assets/images/kidsComponentsImgs/kidsMediaPageImgs/sectionImg16.jpg'
+import sectionImg17 from '../../assets/images/kidsComponentsImgs/kidsMediaPageImgs/sectionImg17.jpg'
+import sectionImg18 from '../../assets/images/kidsComponentsImgs/kidsMediaPageImgs/sectionImg18.jpg'
+import sectionImg19 from '../../assets/images/kidsComponentsImgs/kidsMediaPageImgs/sectionImg19.jpg'
+import sectionImg20 from '../../assets/images/kidsComponentsImgs/kidsMediaPageImgs/sectionImg20.jpg'
+
+const kidsMediaPageData = [
+    {
+        type: 'video',
+        title: 'Luigi’s Mansion 2 HD – Overview Trailer — Nintendo Switch',
+        thumbnail: sectionImg1,
+        backgroundColor: '#1ddb81',
+        color: '#000',
+        gridColumnSpan: 'span 2',
+        gridRowSpan: 'span 1',
+    },
+    {
+        type: 'video',
+        title: 'Paper Mario: The Thousand-Year Door — Launch Trailer — Nintendo Switch',
+        thumbnail: sectionImg2,
+        backgroundColor: '#484848',
+        color: '#fff',
+        gridColumnSpan: 'span 1',
+        gridRowSpan: 'span 2',
+    },
+    {
+        type: 'video',
+        title: 'Endless Ocean Luminous — Launch Trailer — Nintendo Switch',
+        thumbnail: sectionImg3,
+        backgroundColor: '#00d2ff',
+        color: '#000',
+        gridColumnSpan: 'span 2',
+        gridRowSpan: 'span 1',
+    },
+    {
+        type: 'video',
+        title: 'Yoshi & Yoshi & The Egg | LEGO Super Mario',
+        thumbnail: sectionImg4,
+        backgroundColor: '#00d2ff',
+        color: '#000',
+        gridColumnSpan: 'span 1',
+        gridRowSpan: 'span 2',
+    },
+    {
+        type: 'wallpaper',
+        title: 'Underwater scene',
+        thumbnail: sectionImg5,
+        backgroundColor: '#fff',
+        color: '#000',
+        gridColumnSpan: 'span 2',
+        gridRowSpan: 'span 2',
+    },
+    {
+        type: 'wallpaper',
+        title: 'On display!',
+        thumbnail: sectionImg7,
+        backgroundColor: '#d1b193',
+        color: '#000',
+        gridColumnSpan: 'span 2',
+        gridRowSpan: 'span 2',
+    },
+    {
+        type: 'video',
+        title: 'Bowser Gets Bounced! LEGO Super Mario',
+        thumbnail: sectionImg6,
+        backgroundColor: '#ee0500',
+        color: '#fff',
+        gridColumnSpan: 'span 1',
+        gridRowSpan: 'span 2',
+    },
+    {
+        type: 'video',
+        title: 'Princess Peach: Showtime! – Overview Trailer – Nintendo Switch',
+        thumbnail: sectionImg8,
+        backgroundColor: '#fff000',
+        color: '#000',
+        gridColumnSpan: 'span 1',
+        gridRowSpan: 'span 2',
+    },
+    {
+        type: 'video',
+        title: 'Yoshi & Yoshi & The Egg | LEGO Super Mario',
+        thumbnail: sectionImg9,
+        backgroundColor: '#ee0500',
+        color: '#fff',
+        gridColumnSpan: 'span 2',
+        gridRowSpan: 'span 1',
+    },
+    
+    {
+        type: 'wallpaper',
+        title: 'Showstopping wallpaper!',
+        thumbnail: sectionImg10,
+        backgroundColor: '#bc0907',
+        color: '#fff',
+        gridColumnSpan: 'span 2',
+        gridRowSpan: 'span 2',
+    },
+    
+    {
+        type: 'video',
+        title: 'Mario vs. Donkey Kong Trailer — A Heist of Mini Proportions',
+        thumbnail: sectionImg11,
+        backgroundColor: '#ee0500',
+        color: '#fff',
+        gridColumnSpan: 'span 1',
+        gridRowSpan: 'span 2',
+    },
+    {
+        type: 'wallpaper',
+        title: 'Spotlight on Peach',
+        thumbnail: sectionImg12,
+        backgroundColor: '#f8e9d5',
+        color: '#000',
+        gridColumnSpan: 'span 2',
+        gridRowSpan: 'span 2',
+    },
+    {
+        type: 'video',
+        title: 'Fun games with Princess Peach',
+        thumbnail: sectionImg13,
+        backgroundColor: '#00d2ff',
+        color: '#000',
+        gridColumnSpan: 'span 2',
+        gridRowSpan: 'span 1',
+    },
+    {
+        type: 'video',
+        title: 'It’s LEGO Peach time!',
+        thumbnail: sectionImg14,
+        backgroundColor: '#ee0500',
+        color: '#fff',
+        gridColumnSpan: 'span 1',
+        gridRowSpan: 'span 2',
+    },
+    {
+        type: 'wallpaper',
+        title: 'Happy holidays wallpaper',
+        thumbnail: sectionImg15,
+        backgroundColor: '#cbc18e',
+        color: '#000',
+        gridColumnSpan: 'span 2',
+        gridRowSpan: 'span 2',
+    },
+    {
+        type: 'video',
+        title: 'Super Mario RPG – Overview Trailer – Nintendo Switch',
+        thumbnail: sectionImg16,
+        backgroundColor: '#fff000',
+        color: '#000',
+        gridColumnSpan: 'span 2',
+        gridRowSpan: 'span 1',
+    },
+    {
+        type: 'video',
+        title: 'Mario Kart 8 Deluxe - Booster Course Pass Wave 6 - Course Overview',
+        thumbnail: sectionImg17,
+        backgroundColor: '#b42ff5',
+        color: '#fff',
+        gridColumnSpan: 'span 1',
+        gridRowSpan: 'span 2',
+    },
+    {
+        type: 'video',
+        title: 'WarioWare: Move It! – Overview Trailer – Nintendo Switch',
+        thumbnail: sectionImg18,
+        backgroundColor: '#fff000',
+        color: '#000',
+        gridColumnSpan: 'span 1',
+        gridRowSpan: 'span 2',
+    },
+    {
+        type: 'video',
+        title: 'Super Mario Bros. Wonder – Launch Trailer – Nintendo Switch',
+        thumbnail: sectionImg19,
+        backgroundColor: '#ee0500',
+        color: '#fff',
+        gridColumnSpan: 'span 1',
+        gridRowSpan: 'span 2',
+    },
+    {
+        type: 'video',
+        title: 'Detective Pikachu Returns with a Bolt of Brilliance',
+        thumbnail: sectionImg20,
+        backgroundColor: '#fff000',
+        color: '#000',
+        gridColumnSpan: 'span 2',
+        gridRowSpan: 'span 1',
+    },
+]
+
+export default kidsMediaPageData;
